fbff7ca7b89dc3008e28da4c39e7fab085d4e039
Yulia <mavkayulia@gmail.com>
2026-03-03 17:51:46 +0200

    424

--- src/main/kotlin/SlidingWindow/424_LongestRepeatingCharacterReplacement.kt
+++ src/main/kotlin/SlidingWindow/424_LongestRepeatingCharacterReplacement.kt
@@ -3,32 +3,54 @@ package SlidingWindow
 import kotlin.math.max
 
 class `424_LongestRepeatingCharacterReplacement` {
-    fun characterReplacement(s: String, k: Int): Int {
-        var max = 0
-        var wStart = 0
-        var wEnd = 0
-        var temp = 0
-        var myK = k
 
-        while (wEnd < s.length) {
-            if (s[wStart] == s[wEnd]) {
-                temp++
-                wEnd++
-                max = max(max, temp)
-            } else if (s[wStart] != s[wEnd] && myK > 0) {
-                myK--
-                temp++
-                wEnd++
-                max = max(max, temp)
-            } else if (s[wStart] != s[wEnd] && myK == 0) {
-                temp--
+    fun characterReplacement(s: String, k: Int): Int {
+        val map = mutableMapOf<Char, Int>()
+        var res = 0
+        var wStart = 0
+        var maxFr = 0
+
+        for (wEnd in s.indices) {
+            map[s[wEnd]] = (map[s[wEnd]] ?: 0) + 1
+            maxFr = maxOf(maxFr, map[s[wEnd]]!!)
+
+            while (wEnd - wStart + 1 - maxFr > k) {
+                map[s[wStart]] = map[s[wStart]]!! - 1
                 wStart++
-                while (myK < k) {
-                    myK++
-                }
             }
 
+            res = maxOf(res, wEnd - wStart + 1)
         }
-        return max
+
+        return res
     }
+}
+
+fun characterReplacement1(s: String, k: Int): Int {
+    var max = 0
+    var wStart = 0
+    var wEnd = 0
+    var temp = 0
+    var myK = k
+
+    while (wEnd < s.length) {
+        if (s[wStart] == s[wEnd]) {
+            temp++
+            wEnd++
+            max = max(max, temp)
+        } else if (s[wStart] != s[wEnd] && myK > 0) {
+            myK--
+            temp++
+            wEnd++
+            max = max(max, temp)
+        } else if (s[wStart] != s[wEnd] && myK == 0) {
+            temp--
+            wStart++
+            while (myK < k) {
+                myK++
+            }
+        }
+
+    }
+    return max
 }
